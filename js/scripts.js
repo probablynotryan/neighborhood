93e759d4c2cd605ef62bb5ffb96aa6a4d324bcca
Ryan Bass <theryanbass@gmail.com>
2022-01-21 15:15:59 -0800

    condense beepBoop function

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,13 @@
 function beepBoop(inputNumber) {
   let responseArray = [];
   for (let i = 0; i <= inputNumber; i++){
-      if (i === 3) {
+    let num = i.toString().split('');
+    if (num.includes('3')) {
       responseArray.push("Will you be my neighbor?");
-    } else if (i === 2) {
+    } else if (num.includes('2')) {
       responseArray.push("Boop!");
-    } else if (i === 1) {
+    } else if (num.includes('1')) {
       responseArray.push("Beep!");
-    } else if (i == inputNumber) {
-      responseArray.push(i + ".");
-    } else if (i < 10) {
-      responseArray.push(i + ",");
-    } else if (i > 9) {
-      let bigNumber = i.toString().split('');
-      if (bigNumber.includes('3')) {
-        responseArray.push("Will you be my neighbor?");
-      } else if (bigNumber.includes('2')) {
-        responseArray.push("Boop!");
-      } else if (bigNumber.includes('1')) {
-        responseArray.push("Beep!");
-      } else {
-        if (i == inputNumber){
-          responseArray.push(i + ".");
-        } else {
-          responseArray.push(i + ",");
-        }
-      }
     } else {
       if (i == inputNumber){
         responseArray.push(i + ".");
